31df8897a7660215c301748fee7cdeff812d1dad
Plakhota <90543395+Plakhota@users.noreply.github.com>
2025-07-23 08:57:52 +0300

    Update example-images.js

--- test/example-images.js
+++ test/example-images.js
@@ -31,11 +31,11 @@ describe('Eyes-Images', () => {
         // await eyes.check('URL', Target.image('https://i.ibb.co/bJgzfb3/applitools.png'))
         
         const imageBuffer = await fetch('https://i.ibb.co/bJgzfb3/applitools.png').then(resp => resp.buffer())
-        await eyes.check('Buffer', Target.image(imageBuffer))
+        await eyes.check('MAIN Buffer', Target.image(imageBuffer))
 
-        await eyes.check('file path', Target.image(path.resolve(__dirname, 'applitools.png')))
+        // await eyes.check('file path', Target.image(path.resolve(__dirname, 'applitools.png')))
 
-        await eyes.check('base 64 string', Target.image(imageBuffer.toString('base64')))
+        // await eyes.check('base 64 string', Target.image(imageBuffer.toString('base64')))
 
         await eyes.close(false)
     })
